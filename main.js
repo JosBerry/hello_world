e153e98d4e08e68c8d65cc19f5653a9e9de7fa56
JosBerry <josephberry86@yahoo.com>
2019-06-29 18:55:34 -0400

    comment for joseph in the code to improve site

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 (function(){
-    console.log('here we are');
+    /**
+     * Assignment for Joseph:
+     * if the header already says "Hello Guyz" make the header go back to saying "Hello World" 
+     */
     document.querySelector('button').addEventListener('click', function() {
         document.querySelector('h1').innerHTML = 'Hello Guyz';
     });
